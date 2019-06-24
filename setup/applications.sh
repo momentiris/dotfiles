cccfd4770d43a8d4494fc330432a5e3dc9e0f5fe
momentiris <momentiris@gmail.com>
2019-06-24 13:08:01 +0200

    ok

--- setup/applications.sh
+++ setup/applications.sh
@@ -2,11 +2,9 @@ source ~/dotfiles/setup/functions.sh
 
 if ! command -v brew >/dev/null; then
   fancy_echo "Installing Homebrew ..."
-
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   brew tap caskroom/cask
   brew tap caskroom/versions
-
 fi
 
 if brew list | grep -Fq brew-cask; then
@@ -16,15 +14,16 @@ fi
 
 brew update && brew install `brew outdated`
 
-fancy_echo "Installing CLI tools"
+fancy_echo "Installing CLI tools..."
 brew install openssl
 brew install zsh
 brew install zsh-completions
 brew install z
 brew install watchman # needed for jest --watch
 brew install git
+brew install antigen
 
-fancy_echo "Setting up Node with NVM"
+fancy_echo "Setting up Node with NVM..."
 mkdir ~/.nvm
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.1/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
@@ -32,7 +31,7 @@ export NVM_DIR="$HOME/.nvm"
 nvm install node
 nvm alias default node
 
-fancy_echo "Setting up Node with NVM"
+fancy_echo "Setting up misc applications"
 brew tap caskroom/cask
 brew cask install google-chrome
 brew cask install iterm2
